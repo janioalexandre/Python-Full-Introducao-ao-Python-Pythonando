76eccc78231f0f63b40f4dd6df1701ec7ab4b378
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-02-19 23:58:49 -0300

    Entrada e saída de dados

---
+++ 1_variaveis_e_tipos_de_dados.py
@@ -0,0 +1,15 @@
+# Variáveis e tipos de dados
+
+# -> Receber dados
+# -> Armazenar dados
+# -> Processar dados
+# -> Mostrar dados processados
+
+# -> int (inteiro) -10 -5 0 5 10
+# -> float (ponto flutuante - decimal) 0.0 1.5 2.5 3.0
+# -> str (string - Cadeia de caracteres) 'Janio Alexandre' 'Olá' '38' '3.14'
+# -> bool (booleano) True False
+
+idade = 38 
+print(type(idade))
+print(idade)
---
+++ 2_entrada_e_saida_de_dados.py
@@ -0,0 +1,11 @@
+# Entrada e saída de dados
+
+'''
+idade = 38
+nome = 'Janio Alexandre'
+
+print(f'Sua idade é: {idade}')
+'''
+
+idade = input('Digite sua idade: \n')
+print(f'A sua idade é {idade} anos') 
