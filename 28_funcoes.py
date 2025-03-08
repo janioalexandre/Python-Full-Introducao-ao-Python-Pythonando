64a353327a85105ed759bc3b38a32adbd5251950
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-03-07 21:59:58 -0300

    Modularização

---
+++ 28_funcoes.py
@@ -0,0 +1,6 @@
+def soma_valores (n1, n2):
+    soma = n1 + n2
+    return soma
+
+soma = soma_valores(10, 20)
+print(soma)
---
+++ modularizacao/main.py
@@ -0,0 +1,4 @@
+from modularizacao.minhas_funcoes.minha_lib import soma_numeros
+
+x = soma_numeros(1, 2)
+print(x)
